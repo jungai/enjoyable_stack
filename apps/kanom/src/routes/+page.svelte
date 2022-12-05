7a9491e3ff8066a700357a667bce18032dfa2467
jungai <juworaphol125@gmail.com>
2022-12-05 17:34:22 +0700

    refactor(kanom): add example with trpc

--- apps/kanom/src/routes/+page.svelte
+++ apps/kanom/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1 class="text-red-400 text-xl underline font-semibold">Hello</h1>
+<script lang="ts">
+	import { trpc } from '$lib/trpc';
 
+	let username = '';
+	let password = '';
+
+	async function handleOnClick() {
+		const data = await trpc().register.mutate({ username, password });
+		console.log('data ->', data);
+	}
+</script>
+
+<h1 class="text-red-400 text-xl underline font-semibold">Hello</h1>
+<br />
+
+{username}
+<input bind:value={username} class="border border-gray-400" placeholder="enter your username" />
+{password}
+<br />
+<input bind:value={password} class="border border-gray-400" placeholder="enter your password" />
+<br />
+<button class="border border-gray-200 rounded-lg" on:click={handleOnClick}>register </button>
