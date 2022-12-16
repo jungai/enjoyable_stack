1dedd4e189aa798bb54945d8537831b8965cdbf7
jungai <juworaphol125@gmail.com>
2022-12-16 20:45:48 +0700

    feat(kanom): add daisy & example

---
+++ apps/kanom/src/lib/components/TextInput.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let label: string;
+</script>
+
+<div class="form-control w-full max-w-xs">
+	<label class="label">
+		<span class="label-text">{label}</span>
+	</label>
+	<input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+</div>
--- apps/kanom/src/routes/+page.svelte
+++ apps/kanom/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { trpc } from '$lib/trpc';
+	import TextInput from '$lib/components/TextInput.svelte';
 
 	let username = '';
 	let password = '';
@@ -10,13 +11,6 @@
 	}
 </script>
 
-<h1 class="text-red-400 text-xl underline font-semibold">Hello</h1>
-<br />
-
-{username}
-<input bind:value={username} class="border border-gray-400" placeholder="enter your username" />
-{password}
-<br />
-<input bind:value={password} class="border border-gray-400" placeholder="enter your password" />
-<br />
-<button class="border border-gray-200 rounded-lg" on:click={handleOnClick}>register </button>
+<TextInput label={'username'} />
+<TextInput label={'password'} />
+<button class="btn" on:click={handleOnClick}>register</button>
