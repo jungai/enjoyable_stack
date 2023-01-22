07eedcc72bcc01eb9378f3bbe9a44bb5ef8bbc15
jungai <juworaphol125@gmail.com>
2023-01-22 20:18:19 +0700

    fix(auth): import prisma

--- services/auth/src/context.ts
+++ services/auth/src/context.ts
@@ -1,6 +1,6 @@
 import { inferAsyncReturnType } from "@trpc/server";
 import { CreateFastifyContextOptions } from "@trpc/server/adapters/fastify";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from ".prisma/client";
 
 export async function createContext(_ctx: CreateFastifyContextOptions) {
 	const prisma = new PrismaClient();
